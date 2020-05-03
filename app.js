ea4e0d081a7aabc8f9d25ca59861cf14c082d77b
unKnowProgrammer <58310789+unKnowProgrammer@users.noreply.github.com>
2020-05-04 02:26:12 +0300

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = require("express")()
 const pug = require('pug');
 
 // Compile the source code
-const compiler = pug.compileFile('./views/index.pug');
+// const compiler = pug.compileFile('./views/index.pug');
 
 app.get( "/" , (req , res ) => {
 
