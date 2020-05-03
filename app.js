fba1e02fa70ec8130f387fc2417459acd53d183b
unKnowProgrammer <58310789+unKnowProgrammer@users.noreply.github.com>
2020-05-04 02:02:03 +0300

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const pug = require('pug');
 // Compile the source code
 const compiler = pug.compileFile('index.pug');
 
-app.get( "/" , (res , res ) => {
+app.get( "/" , (req , res ) => {
 
 res.send(compiler({test : "Furkan "}))
 }
