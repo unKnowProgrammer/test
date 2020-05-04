ae7cd4aeb4af89ad1453652fb62717029c37508f
unKnowProgrammer <58310789+unKnowProgrammer@users.noreply.github.com>
2020-05-04 09:24:14 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 const express = require("express")
 let app = express();
 const pug = require('pug');
-const compiledFunction = pug.compileFile('index.pug');
+const compiledFunction = pug.compileFile('./Views/index.pug');
 
 app.get("/" , (req , res) => {
     res.send(compiledFunction());
 })
 
-app.listen(process.env.PORT , function(){
-console.log("SERVER STARTED IN HEROKU !!!")
-})
+app.listen(process.env.PORT || 3000 )
