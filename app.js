7612006ec34a44cb6bee671c5670492a7a3bf940
unKnowProgrammer <58310789+unKnowProgrammer@users.noreply.github.com>
2020-05-04 08:47:12 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require("express")
 let app = express();
 const pug = require('pug');
+const compiledFunction = pug.compileFile('./views/index.pug');
 
 app.get("/" , (req , res) => {
-    res.send(pug.renderFile("./views/index.pug"));
+    res.send(compiledFunction());
 })
 
 app.listen(process.env.PORT)
