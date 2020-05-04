a8df8421d0d7745ce8ddc8c3da143b9b8e309c2b
unKnowProgrammer <58310789+unKnowProgrammer@users.noreply.github.com>
2020-05-04 09:06:39 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express")
 let app = express();
 const pug = require('pug');
-const compiledFunction = pug.compileFile('./views/index.pug');
+const compiledFunction = pug.compileFile('index.pug');
 
 app.get("/" , (req , res) => {
     res.send(compiledFunction());
